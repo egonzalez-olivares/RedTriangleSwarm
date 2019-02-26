582c61a12bc404b2ef0c625ef367caeae794dd70
Jessica Ubaldo <jubaldo@csumb.edu>
2019-02-26 15:33:17 -0800

    triangle added

--- Red Triangle Swarm/Assets/Scripts/TriangleMove.cs
+++ Red Triangle Swarm/Assets/Scripts/TriangleMove.cs
@@ -6,14 +6,12 @@ public class TriangleMove : MonoBehaviour {
 
     public GameObject player;
     public float speed;
-
-	// Use this for initialization
+    
 	void Start () {
         player = GameObject.FindGameObjectWithTag("Player");
         speed = 1f;
 	}
 	
-	// Update is called once per frame
 	void Update () {
         transform.forward = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z);
         transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
